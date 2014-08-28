4970a689c80902456246bcd8c84ef05c061fdbbd
malmsteen08 <neziryurekli@gmail.com>
2014-08-28 13:57:25 +0300

    fix field

--- MockupCustomTable/MockupCustomTable/Scripts/TableFieldList.js
+++ MockupCustomTable/MockupCustomTable/Scripts/TableFieldList.js
@@ -2,7 +2,7 @@
     $('tr[data-id="8E939E0C64F34ECCAA85CB9CE0554FDE"]').prepend($('<td>false</td>'));
     $('tr[data-id="8E939E0C64F34ECCAA85CB9CE0554FDE"]').prepend($('<td>' + $.cookie('TableHelptext') + '</td>'));
     $('tr[data-id="8E939E0C64F34ECCAA85CB9CE0554FDE"]').prepend($('<td></td>'));
-    $('tr[data-id="8E939E0C64F34ECCAA85CB9CE0554FDE"]').prepend($('<td>' + $.cookie('TableFieldName') + '</td>'));
+    $('tr[data-id="8E939E0C64F34ECCAA85CB9CE0554FDE"]').prepend($('<td>' + $.cookie('AddTableFieldName') + '</td>'));
     $('tr[data-id="8E939E0C64F34ECCAA85CB9CE0554FDE"]').prepend($('<td style="font-family: consolas;">' + $.cookie('TableName') + '</td>'));
     $('tr[data-id="8E939E0C64F34ECCAA85CB9CE0554FDE"]').prepend($('<td class="index">2</td>'));
 });
