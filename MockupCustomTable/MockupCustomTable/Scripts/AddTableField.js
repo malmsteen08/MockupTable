e96d55c655d556d596125c866fbeafe6fc2ef2a9
malmsteen08 <neziryurekli@gmail.com>
2014-08-28 16:17:01 +0300

    added files

---
+++ MockupCustomTable/MockupCustomTable/Scripts/AddTableField.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('tr[data-id="8E939E0C64F34ECCAA85CB9CE0554FED"]').prepend($('<td>false</td>'));
+    $('tr[data-id="8E939E0C64F34ECCAA85CB9CE0554FED"]').prepend($('<td>' + $.cookie('AddTableHelptext') + '</td>'));
+    $('tr[data-id="8E939E0C64F34ECCAA85CB9CE0554FED"]').prepend($('<td></td>'));
+    $('tr[data-id="8E939E0C64F34ECCAA85CB9CE0554FED"]').prepend($('<td>' + $.cookie('AddTableFieldName') + '</td>'));
+    $('tr[data-id="8E939E0C64F34ECCAA85CB9CE0554FED"]').prepend($('<td style="font-family: consolas;">' + $.cookie('AddTableName') + '</td>'));
+    $('tr[data-id="8E939E0C64F34ECCAA85CB9CE0554FED"]').prepend($('<td class="index">2</td>'));
+});
